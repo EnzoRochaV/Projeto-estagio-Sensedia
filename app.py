d256662da86e5c18858006f8419004dc540a261b
Enzo Rocha Viri <enzorochavirii@hotmail.com>
2026-01-14 21:33:31 -0300

    Ajustes no cadastro de usuário e validação de termo de usuario

--- app.py
+++ app.py
@@ -4,24 +4,56 @@ con = sqlite3.connect("sistema.db")
 
 cursor = con.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS usuario(nome varchar(50), data int, telefone int)')
 
-nome = input('Qual o seu nome?' )
-data = input('Qual sua data de aniversario?')
-telefone = input('Qual seu numero de telefone?')
+#criação das tabelas para o banco de dados, somente se elas já não foram criadas
+cursor.execute('CREATE TABLE IF NOT EXISTS usuario (nome TEXT, data_nascimento TEXT, telefone TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS projeto (nome_do_projeto TEXT, data_de_inicio TEXT, data_de_finalização TEXT)')
 
-verif = cursor.execute('SELECT nome FROM usuario')
+#função para o inicio do processo de cadastro do usuario, usando a validação do termo de usuario
+def iniciar_cadastro_usuario():
+    print('\nInsira os dados abaixo para iniciar o cadastro')
 
-usuarios = verif.fetchall()
+    nome = input('\nQual o seu nome?' ).lower()
+    data = input('\nQual sua data de nascimento?').lower()
+    telefone = input('\nQual seu numero de telefone?').lower()
 
-nomes_lp = [i[0] for i in usuarios] 
+    print('\nPara prosseguir com o cadastro, você deve aceitar os termos de uso.')
 
-if nome in nomes_lp :
-    print('Usuario ja cadastrado')
+    termo = input('\nVocê aceita os termos de uso? (s/n): ').lower()
 
-else : 
-    cursor.execute('INSERT INTO usuario VALUES (?,?,?)',(nome,data,telefone))
-    print('Cadastro executado com sucesso')
+    if termo != 's':
+        print('\nCadastro não concluído. É necessário aceitar os termos de uso.')
+        con.close()
+        exit()
+
+
+    verif = cursor.execute('SELECT nome FROM usuario')
+
+    usuarios = verif.fetchall()
+
+    nomes_lp = [i[0] for i in usuarios] 
+
+    if nome in nomes_lp :
+        print('\nUsuario ja cadastrado')
+        recomeco = input('\nDeseja recomeçar o cadastro? (s/n)').lower()  
+            
+        if recomeco == 's':
+            
+                iniciar_cadastro_usuario()
+            
+        else:
+            
+            print('\nCadastro encerrado.')   
+            con.close()
+            exit()
+
+
+    else : 
     
+        cursor.execute('INSERT INTO usuario VALUES (?,?,?)',(nome,data,telefone))
+        print('\nCadastro executado com sucesso')
+    
+iniciar_cadastro_usuario()
 
 con.commit()
+con.close() 
